30cba75c92e47cfd8db13602c4fca35dafa6c086
sc-bin <3335447573@qq.com>
2025-03-12 14:54:51 +0800

    改为在安装时编译驱动

---
+++ compile_driver.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "开始编译"
+
+
+KERNEL="/usr/lib/modules/$(uname -r)/build/"
+echo "KERNEL: $KERNEL"
+if [ ! -d $KERNEL ]; then
+    echo " $KERNEL not exist"
+    exit
+fi
+
+
+echo "compile drivers"
+make -C ${PATH_DRIVER}
+if [ -d $PATH_DRIVER_SAVE ]; then
+    rm -r $PATH_DRIVER_SAVE
+fi
+mkdir -p $PATH_DRIVER_SAVE
+cp -r ${PATH_DRIVER}/* $PATH_DRIVER_SAVE
+
+echo -e "\nupdate system drivers...."
+depmod
