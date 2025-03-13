73f84aa90238ea16e846245e18e8e318bcc79c3e
sc-bin <3335447573@qq.com>
2025-03-13 14:53:09 +0800

    优化在构建镜像时编译

--- compile_driver.sh
+++ compile_driver.sh
@@ -6,9 +6,16 @@ if ! ls -d /usr/lib/modules/*/ > /dev/null 2>&1; then
     return
 fi
 
+PATH_PWD="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+PATH_DRIVER="${PATH_PWD}/drivers"
+MODULE_PATH="$(ls -d /lib/modules/* | head -n 1)"
+
+PATH_DRIVER_SAVE="${MODULE_PATH}/kernel/wpi-drivers/"
+
 
 echo "compile drivers"
 make -C ${PATH_DRIVER}
+echo "编译完成"
 if [ -d $PATH_DRIVER_SAVE ]; then
     rm -r $PATH_DRIVER_SAVE
 fi
