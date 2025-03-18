915c3c0a14c7ddb0cbdeb1e44bf101616e48dc29
sc-bin <3335447573@qq.com>
2025-03-18 16:38:48 +0800

    优化启用了但没插mipi屏时的逻辑

--- auto_choose.sh
+++ auto_choose.sh
@@ -15,6 +15,14 @@ now_screen=$(get_now_screen)
 config_screen=$(get_config_screen)
 # echo "当前  $now_screen "
 # echo "配置  $config_screen "
+
+# t527上如果设置为dsi屏幕，但dsi屏幕通讯失败了，会导致后续其他驱动报错死机
+if [[ "$now_screen" == dsi* ]]; then
+    if dmesg | grep -q "panel-dsi driver not probe"; then
+        config_screen="hdmi"
+    fi
+fi
+
 if [ "$now_screen" == "$config_screen" ]; then
     # echo "无事发生"
     exit
